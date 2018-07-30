d6b12ddcbfa662e5b211c958521a5b7a26b204f9
jackbuehner <jackebuehner@gmail.com>
2018-07-30 18:32:07 -0400

    new app

---
+++ main.js
@@ -0,0 +1,75 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const {ipcMain} = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+let secondWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    height: 1000,
+    width: 1200,
+	frame: false,
+    webPreferences: {
+      experimentalFeatures: true
+    }
+  })
+
+  secondWindow = new BrowserWindow({
+    height: 800,
+    width: 1100,
+	frame: false,
+	show: true,
+    webPreferences: {
+      experimentalFeatures: true
+    }
+  })
+	
+  secondWindow.loadFile('./docs/app_center.html')
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('./docs/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+ipcMain.on('open-second-window', (event, arg)=> {
+    secondWindow.show()
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
